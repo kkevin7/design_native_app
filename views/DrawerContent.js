1e30114a5b7a9ba24168a847a9e789bf3700d9c7
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-27 16:38:33 -0600

    User and Avatar section

--- views/DrawerContent.js
+++ views/DrawerContent.js
@@ -1,15 +1,102 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+//React Native Paper
+import {
+  Avatar,
+  Title,
+  Caption,
+  Paragraph,
+  Drawer,
+  Text,
+  TouchableRipple,
+  Switch,
+} from 'react-native-paper';
 
-const DrawerContent = () => {
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const DrawerContent = (props) => {
   return (
     <>
-      <View>
-        <Text>DrawerContent</Text>
+      <View style={{flex:1}}>
+        <DrawerContentScrollView {...props}>
+            <View style={styles.drawerContent}>
+                <View style={styles.userInfoSection}>
+                    <View style={{flexDirection: 'row', marginTop: 15}}>
+                        <Avatar.Image
+                            source={{
+                                uri: `https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/ab/Iron_Man_Mark_LXXXV.png/revision/latest/top-crop/width/360/height/450?cb=20190405192851&path-prefix=es`
+                            }}
+                            size={50}
+                         />
+                         <View style={{marginLeft: 15, flexDirection: 'column'}}>
+                             <Title style={styles.title}>Kevin Martínez</Title>
+                             <Caption style={styles.caption}>@kevinmartinez7</Caption>
+                         </View>
+                    </View>
+                </View>
+            </View>
+        </DrawerContentScrollView>
+        <Drawer.Section style={styles.bottomDrawerSection}>
+            <DrawerItem
+                icon={(color, size) => <Icon 
+                    name="exit-to-app"
+                    color={color}
+                    size={size}
+                />}
+                label="Sign Out"
+                onPress={() => {}}
+            />
+        </Drawer.Section>
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: 'bold',
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  bottomDrawerSection: {
+    marginBottom: 15,
+    borderTopColor: '#f4f4f4',
+    borderTopWidth: 1,
+  },
+  preference: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+});
 
 export default DrawerContent;
